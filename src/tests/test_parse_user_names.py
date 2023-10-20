85fa4523f3b292f0fb1ea0d4e749be24b465bd98
Simon <31246246+SimonMolinsky@users.noreply.github.com>
2023-10-20 09:54:01 +0200

    black reformat

--- src/tests/test_parse_user_names.py
+++ src/tests/test_parse_user_names.py
@@ -3,7 +3,6 @@ from pyosmeta.parse_issues import parse_user_names
 
 
 class TestParseUserNames(TestCase):
-
     def setUp(self) -> None:
         self.name1 = "Test User (@test1user)"
         self.name2 = "(@test2user)"
@@ -11,29 +10,17 @@ class TestParseUserNames(TestCase):
         self.name4 = "@test4user"
 
     def test_parse_1(self):
-        d = {
-            "name": "Test User",
-            "github_username": "test1user"
-        }
+        d = {"name": "Test User", "github_username": "test1user"}
         self.assertDictEqual(d, parse_user_names(self.name1))
 
     def test_parse_2(self):
-        d = {
-            "name": "",
-            "github_username": "test2user"
-        }
+        d = {"name": "", "github_username": "test2user"}
         self.assertDictEqual(d, parse_user_names(self.name2))
 
     def test_parse_3(self):
-        d = {
-            "name": "Test user 3",
-            "github_username": "test3user"
-        }
+        d = {"name": "Test user 3", "github_username": "test3user"}
         self.assertDictEqual(d, parse_user_names(self.name3))
 
     def test_parse_4(self):
-        d = {
-            "name": "",
-            "github_username": "test4user"
-        }
+        d = {"name": "", "github_username": "test4user"}
         self.assertDictEqual(d, parse_user_names(self.name4))
